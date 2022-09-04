26297c0727676b70fc162802dd86a663ce08f029
wesley <88048019@qq.com>
2022-09-04 09:02:01 +0000

    Mysql config

--- meiduo_mall/meiduo_mall/settings.py
+++ meiduo_mall/meiduo_mall/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'meiduo_mall.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST':'localhost',
+        'PORT':3306,
+        'USER':'wesley',
+        'PASSWORD':'Xyz!1234',
+        'NAME': 'meiduo_mall',
     }
 }
 
